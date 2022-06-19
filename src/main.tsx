3c229ba4b6fcb9189974793435e97ba7e00cd7e7
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-06-19 11:30:34 -0500

    use userProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import { UserProvider } from "./context/provider/UserProvider";
+import "./index.scss";
 import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-     <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
