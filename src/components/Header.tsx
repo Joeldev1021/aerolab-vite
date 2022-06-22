4114955afd7d2295b6a59c527fb688ae7c76dbdd
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-06-22 16:55:42 -0500

    feat : use addCoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,36 @@
 import {
   Box,
   Button,
-  ButtonGroup,
   Flex,
   Heading,
-  Spacer
+  Img,
+  Spacer,
+  Text
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import Banner from "./Banner";
 
+const valueCoints = 100;
+
 const Header = () => {
+  const { user, addCoint } = useContext(UserContext);
+
+  const handleClick = () => {
+    addCoint(valueCoints);
+  };
+
   return (
     <>
-      <Flex minWidth="max-content" alignItems="center" gap="2">
+      <Flex minWidth="max-content" alignItems="center" padding={2} px={4} gap="2">
         <Box p="2">
-          <Heading size="md">Chakra App</Heading>
+          <Heading size="md"><Img src="./assets/logo.svg" alt="" /></Heading>
         </Box>
         <Spacer />
-        <ButtonGroup gap="2">
-          <Button colorScheme="teal">Sign Up</Button>
-          <Button colorScheme="teal">Log in</Button>
-        </ButtonGroup>
+        <Box gap="2" display="flex" alignItems="center" >
+          <Text>{user.name}</Text>
+          <Button onClick={() => handleClick()} >{user.points}<img src="./assets/coin.svg" alt="coin" /></Button>
+        </Box>
       </Flex>
       <Banner />
     </>
