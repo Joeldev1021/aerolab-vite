3a51b1ff6a3564584853fd4caaea8feb8e70d0c8
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-02 08:33:40 -0500

    fix: detail card product

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,30 +1,32 @@
-import { Box, Button, GridItem, Image, Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, GridItem, Image, Img, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import { Product } from "../types";
 
 interface Props {
   product: Product;
 }
-
 const CardProduct = ({ product }: Props) => {
+  const { user } = useContext(UserContext);
+
   return (
-    <GridItem>
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-        <Image src={product.img.url} alt={product.name} />
+    <GridItem className="grid-item" position="relative" margin="auto" p="1" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" >
+      {user.points >= product.cost
+        ? <Img position="absolute" top="3" right="3" src="./assets/buy-blue.svg"/>
+        : <Text borderRadius={50} background="blackAlpha.600" display="flex" alignItems="center" padding={2} px="3" position="absolute" top="3" right="3" color="white">you need {product.cost} <Img src="./assets/coin.svg"/></Text> }
+      <Box >
+        <Box width="95%" margin="auto" borderBottom="1px" borderColor="gray.100" >
+          <Image src={product.img.url} alt={product.name} />
+        </Box>
         <Box p="6">
-          <Box
-            mt="1"
-            fontWeight="semibold"
-            as="h4"
-            lineHeight="tight"
-            noOfLines={1}
-          >
-            <Text color="black">{product.name}</Text>
-          </Box>
-          <Text color="black">{product.cost}</Text>
-          <Button colorScheme="teal" size="md">
-            Button
-          </Button>
+            <Text textAlign="left" color="black">{product.name}</Text>
+            <Text textAlign="left" color="black">{product.category}</Text>
+        </Box>
+      </Box>
+      <Box className="content">
+        <Box width="90%" className="content-info">
+          <Text display="flex" alignItems="center" padding={0} justifyContent="center" fontSize={35}>{product.cost} <Img src="./assets/coin.svg" paddingTop="5px" /></Text>
+          <Button background="white" borderRadius={30} color="blackAlpha.700" width="95%">Redeem</Button>
         </Box>
       </Box>
     </GridItem>
