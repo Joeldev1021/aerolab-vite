06dd243cb29cfefc888636244ff241df4e05a4e0
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-05 15:30:33 -0500

    feat: 🚧 more actions user

--- src/context/actions/UserAction.tsx
+++ src/context/actions/UserAction.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { User } from "../../types";
+import { Product, User } from "../../types";
 
 export type UserAction =
 |{type: "LOAD_USER", payload: User}
-|{type: "REDEEM_PRODUCT"}
-| {type: "HIGHEST_PRICE"}
+|{type: "ADD_COINS", payload: number}
+|{type: "DECREMENT_COINS", payload: {cost:number}}
+| {type: "ADD_REDEEM_HISTORY", payload: {product:Product}}
