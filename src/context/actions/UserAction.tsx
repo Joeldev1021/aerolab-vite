b92e2debc2c74e0bf49dc607e23e34f2b61974a8
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-06 16:18:52 -0500

    feat: 🚧 upgrade userAction

--- src/context/actions/UserAction.tsx
+++ src/context/actions/UserAction.tsx
@@ -2,7 +2,11 @@
 import { Product, User } from "../../types";
 
 export type UserAction =
-|{type: "LOAD_USER", payload: User}
-|{type: "ADD_COINS", payload: number}
+|{type: "LOAD_USER"}
+|{type: "LOAD_USER_SUCCESS", payload: User}
+|{type: "LOAD_USER_ERROR"}
+| {type: "LOAD_ADD_POINTS"}
+|{type: "LOAD_ADD_POINTS_SUCCESS", payload: number}
+| {type: "LOAD_ADD_POINTS_ERROR"}
 |{type: "DECREMENT_COINS", payload: {cost:number}}
 | {type: "ADD_REDEEM_HISTORY", payload: {product:Product}}
