bda06dcd90c80774099889f20a4e87ea2964bd89
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-10 09:21:49 -0500

    fix: home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,13 @@
-/* eslint-disable no-unused-vars */
-import React, { useContext, useEffect, useState } from "react";
-import api from "../api";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ListProducts from "../components/ListProducts";
-import { Product } from "../types";
+import Main from "../components/Main";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Main/>
        <ListProducts />
       <Footer/>
     </div>
