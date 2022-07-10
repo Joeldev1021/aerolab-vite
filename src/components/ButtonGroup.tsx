d140bcadefb700bc860f2668ad9f21c8645077de
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-10 09:14:04 -0500

    fix: 🐛 next pages

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -1,30 +1,34 @@
 import { ButtonGroup } from "@chakra-ui/react";
+import { useEffect } from "react";
 import Arrow from "./Arrow";
 
 interface Props {
   nextPage: number;
   setNextPage: (value: number)=>void;
+  length: number;
 }
 
-const ButtonGroupPage = ({ nextPage, setNextPage }: Props) => {
+const ButtonGroupPage = ({ nextPage, setNextPage, length }: Props) => {
+  /// restart the page
+  useEffect(() => {
+    if (length <= 16) setNextPage(0);
+  }, [length]);
+
+  const handleNextPage = (dir:string) => {
+    if (length <= 16) {
+      setNextPage(0);
+    } else {
+      dir === "right" ? setNextPage(1) : setNextPage(0);
+    }
+  };
   return (
     <ButtonGroup className="btn-group">
-      {nextPage === 0
-        ? (
-        <button onClick={() => setNextPage(1)}>
-          <Arrow dir="right" />
-        </button>
-          )
-        : (
-        <>
-          <button onClick={() => setNextPage(0)}>
+          <button onClick={() => handleNextPage("left")}>
             <Arrow dir="left" />
           </button>
-          <button onClick={() => setNextPage(1)}>
+          <button onClick={() => handleNextPage("right")}>
             <Arrow dir="right" />
           </button>
-        </>
-          )}
     </ButtonGroup>
   );
 };
