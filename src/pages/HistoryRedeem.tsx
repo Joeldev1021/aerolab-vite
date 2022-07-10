8d6c30e6c1041ad26aac141b3a3391560d3ccedc
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-10 09:20:04 -0500

    feat: page historyRedeem

--- src/pages/HistoryRedeem.tsx
+++ src/pages/HistoryRedeem.tsx
@@ -1,18 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { UserContext } from "../context/UserContext";
-import { Product } from "../types";
+import ListHistoryRedeem from "../components/historyRedeem/ListHistoryRedeem";
 
 const HistoryRedeem = () => {
-  const { user } = useContext(UserContext);
   return (
     <div>
         <Header/>
-        {user.redeemHistory.map((product:Product) => {
-          return (
-                <p key={product._id}>{product.name}</p>
-          );
-        })}
+         <ListHistoryRedeem/>
+        <Footer/>
     </div>
   );
 };
