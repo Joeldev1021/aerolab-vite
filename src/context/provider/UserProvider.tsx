431248318e9c85a4a5630178a2d0435eab149954
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-13 18:24:56 -0500

    fix: post points

--- src/context/provider/UserProvider.tsx
+++ src/context/provider/UserProvider.tsx
@@ -15,11 +15,10 @@ export const UserInitial:UserState = {
 
 export const UserProvider = ({ children }:ProviderProps) => {
   const [state, dispatch] = useReducer(ReducerUser, UserInitial);
-
   /**
    * Load user from the API and dispatch the result to the reducer.
    */
-  const loadUser = async () => {
+  const getLoadUser = async () => {
     dispatch({ type: "LOAD_USER" });
     try {
       const user:User = await api.loadUser();
@@ -30,16 +29,40 @@ export const UserProvider = ({ children }:ProviderProps) => {
   };
 
   const addPoints = async (points:number) => {
-    // const res = await api.postCoins(100);
-    dispatch({ type: "LOAD_ADD_POINTS_SUCCESS", payload: points });
+    dispatch({ type: "LOAD_ADD_POINTS" });
+    const res = await api.postCoins(points);
+    // add points
+    try {
+      if (res["new Points"]) {
+        dispatch({ type: "LOAD_ADD_POINTS_SUCCESS", payload: res["new Points"] });
+      } else throw new Error("not add points");
+    } catch (error) {
+      // error add points
+      dispatch({ type: "LOAD_ADD_POINTS_ERROR" });
+    }
+    return res;
   };
 
-  const addRedeemHistory = (product:Product) => {
-    dispatch({ type: "LOAD_ADD_REDEEM_HISTORY_SUCCESS", payload: { product } });
+  /**
+ * This function is called when a user clicks on a product to redeem it. It dispatches an action to the
+ * reducer to update the state, and then it calls the api to add the product to the user's redeem
+ * history.
+ * @param {Product} product - Product
+ * @returns The return value is the result of the dispatch function.
+ */
+  const addRedeemHistory = async (product:Product) => {
+    dispatch({ type: "LOAD_ADD_REDEEM_HISTORY" });
+    const redeemHst = await api.postRedeemHistory(product._id);
+    try {
+      dispatch({ type: "LOAD_ADD_REDEEM_HISTORY_SUCCESS", payload: { product } });
+    } catch (error) {
+      dispatch({ type: "LOAD_ADD_REDEEM_HISTORY_ERROR" });
+    }
+    return redeemHst;
   };
 
   useEffect(() => {
-    loadUser();
+    getLoadUser();
   }, []);
 
   return (
