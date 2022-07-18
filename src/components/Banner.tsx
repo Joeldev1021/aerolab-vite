153236692dab2ff21425e12c0396e4d96d6436de
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-18 13:56:56 -0500

    fix: fix styles banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import { Box, Img, Stack, Text } from "@chakra-ui/react";
 
 const Banner = () => {
   return (
-    <Stack pos="relative" height="450" overflow="hidden">
+    <Stack pt="15" pos="relative" overflow="hidden">
       <Img src="./assets/header.png" alt="header-img" />
       <Box pos="absolute" bottom="10" left="150">
         <Text fontWeight="bold" color="white" fontSize={50}>
