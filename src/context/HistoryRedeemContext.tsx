d00d915c09c4716c336f0d1ccae521773c3c0a47
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-18 11:53:07 -0500

    fix: udgrade type and loadHistory

--- src/context/HistoryRedeemContext.tsx
+++ src/context/HistoryRedeemContext.tsx
@@ -2,9 +2,10 @@ import { createContext } from "react";
 import { Product } from "../types";
 
 export type HistoryRedeemContextType = {
-   historyRedeem: Product[]
+   historyRedeems: Product[]
    isLoading:boolean
    isError:boolean
+   loadHistory: () => void
 }
 
 export const HistoryRedeemContext = createContext<HistoryRedeemContextType>({} as HistoryRedeemContextType);
