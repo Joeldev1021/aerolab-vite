f942bc925f484a0a37ada02d36010d07f29662d1
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-20 11:28:31 -0500

    fix: ⚓ new color

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,8 @@ import { extendTheme } from "@chakra-ui/react";
 const colors = {
   primary: "#0070f3",
   secondary: "#0DD4FA",
-  tertiary: "#f50057"
+  tertiary: "#f50057",
+  grayCustom: "#B8B8B8"
 };
 
 const fonts = {
