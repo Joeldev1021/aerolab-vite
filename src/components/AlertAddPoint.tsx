6154fd3d4449b75951c5d04ad739268d4587cd4c
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-20 11:27:40 -0500

    fix: change styles alert

--- src/components/AlertAddPoint.tsx
+++ src/components/AlertAddPoint.tsx
@@ -2,10 +2,10 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const AlertAddPoint = ({ message }:any) => {
   return (
-    <Box boxShadow='lg' border="2px" borderColor='secondary' p='6' rounded='md' bg='white' display="flex" justifyContent="space-around" alignItems="center">
+    <Box boxShadow='lg' border="2px" borderColor={message.error ? "red" : "secondary"} p='6' rounded='md' bg='white' display="flex" justifyContent="space-around" alignItems="center">
         <Box >
-          <Text fontWeight="bold">{message.error ? "Error" : "¡Congrulations!"}</Text>
-          <Text >{message.error ? message.error : `Update Points ${message["new Points"]}`}</Text>
+          <Text fontWeight="bold">{message.error ? "¡Error!" : "¡Congrulations!"}</Text>
+          <Text >{message.error ? message.error : `Update Points ${message["New Points"]}`}</Text>
         </Box>
     </Box>
   );
