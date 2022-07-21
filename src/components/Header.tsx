38ffaf10e88a39aed7a4271e8bc5f9269e88aff5
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-21 09:14:29 -0500

    feat: upgrade header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,28 @@
 /* eslint-disable no-unused-vars */
 import {
   Box,
-  Button,
   Flex,
   Heading,
   Img,
   Spacer,
-  Spinner,
   Text,
+  useMediaQuery,
   useToast
 } from "@chakra-ui/react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import { AlertAddPoint } from "./AlertAddPoint";
-
-const valuePoints = 100;
+import ModalPoints from "./ModaPoints/ModalPoints";
 
 const Header = () => {
   const { user, addPoints, loading } = useContext(UserContext);
   const toast = useToast();
 
-  const handleClick = async () => {
-    /*  addCoins(valueCoints) */;
-    const res = await addPoints(valuePoints);
+  const [isLargeThat500] = useMediaQuery("(min-width: 640px)");
+
+  const handlePoints = async (points: number) => {
+    const res = await addPoints(points);
     toast({
       duration: 2000,
       position: "bottom-left",
@@ -34,21 +33,18 @@ const Header = () => {
   };
 
   return (
-      <Flex position="fixed" shadow="base" zIndex="100" width="100%" bg="white" alignItems="center" padding={2} px={4} gap="2">
+      <Flex position="fixed" shadow="base" zIndex="100" width="100%" bg="white" alignItems="center" padding={2} px={ isLargeThat500 ? 10 : 1} gap="2">
         <Box p="2" >
           <Link to="/"><Heading size="md"><Img src="./assets/logo.svg" alt="" /></Heading></Link>
         </Box>
         <Spacer />
         <Box gap="5" display="flex" alignItems="center" >
-          <Text fontSize="20">{user?.name}</Text>
-          <Link to="/historyRedeem"><Text fontSize="20" _hover={{ color: "secondary" }}>Redeem History</Text></Link>
-          <Button
-          display="flex"
-          justifyContent="space-between"
-           minWidth="100px"
-          onClick={() => handleClick()} >
-            { loading ? <Spinner size='sm' color="secondary"/> : user.points}<img src="./assets/coin.svg" alt="coin" />
-            </Button>
+
+       <Link to="/">
+         <Text _hover={{ color: "secondary" }} display={isLargeThat500 ? "block" : "none"} fontSize={{ base: "18px", md: "20px" }}>{user?.name}</Text>
+       </Link>
+          <Link to="/historyRedeem"><Text fontSize={{ base: "18px", md: "20px" }} _hover={{ color: "secondary" }}>Redeem History</Text></Link>
+           <ModalPoints loading={loading} points={user.points} handlePoints={handlePoints} />
         </Box>
       </Flex>
   );
