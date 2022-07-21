be4a8ff287092ba7fbbd5e49efd444ec63d4f7fa
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-21 09:13:43 -0500

    fix: 🌠 styles mobile

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,12 @@
-import { Box, Img, Stack, Text } from "@chakra-ui/react";
+import { Box, Img, Stack, Text, useMediaQuery } from "@chakra-ui/react";
 
+// 10 - 150
 const Banner = () => {
   return (
-    <Stack pt="15" pos="relative" overflow="hidden">
+    <Stack pt={{ base: 30, md: 15 }} pos="relative" overflow="hidden">
       <Img src="./assets/header.png" alt="header-img" />
-      <Box pos="absolute" bottom="10" left="150">
-        <Text fontWeight="bold" color="white" fontSize={50}>
+      <Box pos="absolute" bottom={{ base: 2, md: 10 }} left={{ base: "50", md: "150" }} >
+        <Text fontWeight="bold" color="white" fontSize={{ base: 30, md: 50 }} >
           Electronic
         </Text>
       </Box>
